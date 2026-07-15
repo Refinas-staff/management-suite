fde2cd7a9bf842a19fd09cc663b0c7a446f15fb7
Refinas-staff <refinas.shuukei2@gmail.com>
2026-07-15 13:59:55 +0900

    Add files via upload

---
+++ lost-ipad-sw.js
@@ -0,0 +1,24 @@
+const CACHE = 'lost-ipad-flat-v1';
+const ASSETS = [
+  './lost-ipad.html',
+  './lost-ipad.webmanifest',
+  './lost-ipad-favicon.png',
+  './lost-ipad-apple-touch-icon.png',
+  './lost-ipad-icon-192.png',
+  './lost-ipad-icon-512.png'
+];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', event => {
+  const url = new URL(event.request.url);
+  if (url.origin !== self.location.origin || event.request.method !== 'GET') return;
+  event.respondWith(fetch(event.request).then(response => {
+    const copy = response.clone();
+    caches.open(CACHE).then(cache => cache.put(event.request, copy));
+    return response;
+  }).catch(() => caches.match(event.request)));
+});
